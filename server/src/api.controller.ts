c40e55bef03fbcec718b85c21c28de602524fba9
LucasB <lucas_benjaminson@hotmail.com>
2022-08-22 11:23:59 +0200

    PUT now works

--- server/src/api.controller.ts
+++ server/src/api.controller.ts
@@ -1,5 +1,13 @@
 import { Request, Response } from "express";
-import { boardGames } from "./server";
+
+const boardGames: BoardGame[] = [];
+
+interface BoardGame {
+  id: string;
+  name: string;
+  description: string;
+  published: number;
+}
 
 export function getAllObjects(req: Request, res: Response) {
   res.status(200).json(boardGames);
@@ -22,4 +30,14 @@ export const postObject = (req: Request, res: Response) => {
 
 export function deleteAllObjects(req: Request, res: Response) {
   res.status(200).json(boardGames.splice(0, boardGames.length));
+}
+
+export function updateObject(req: Request, res: Response) {
+  const id = req.params.id;
+  const boardGame = boardGames.find((x) => x.id === id);
+  if (boardGame) {
+    const boardGameIndex = boardGames.indexOf(boardGame);
+    boardGames[boardGameIndex] = { id, ...req.body };
+    res.status(200).json(boardGames[boardGameIndex]);
+  } else res.status(204).json("Board game not found");
 }
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,26 +1,19 @@
 import express from "express";
 import {
+  deleteAllObjects,
   getAllObjects,
   getObjectById,
   postObject,
-  deleteAllObjects,
+  updateObject,
 } from "./api.controller";
 
 const app = express();
 app.use(express.json());
 
-export const boardGames: BoardGame[] = [];
-
-interface BoardGame {
-  id: string;
-  name: string;
-  description: string;
-  published: number;
-}
-
 app.get("/api", getAllObjects);
 app.get("/api/:id", getObjectById);
 app.post("/api", postObject);
 app.delete("/api", deleteAllObjects);
+app.put("/api/:id", updateObject);
 
 app.listen(3000, () => console.log("Running on http://localhost:3000"));
